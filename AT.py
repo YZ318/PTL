a41ae2cf922f785e894d65ccba2367d70f93b79b
YZ318 <YakovZhuravlev88@gmail.com>
2023-09-21 19:21:22 +0300

    промежуточная аттестация

--- AT.py
+++ AT.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import random
 
-lst = ['robot'] * 10
-lst += ['human'] * 10
+lst = ["robot"] * 10
+lst += ["human"] * 10
 random.shuffle(lst)
 
-data = pd.DataFrame({'whoAmI': lst})
-data_encoded = pd.get_dummies(data['whoAmI'])
+data = pd.DataFrame({"whoAmI": lst})
+data_encoded = pd.get_dummies(data["whoAmI"])
 
 # Объединяем исходный DataFrame с преобразованным one-hot столбцами
 data_encoded = pd.concat([data, data_encoded], axis=1)
-data_encoded = data_encoded.drop('whoAmI', axis=1)
+data_encoded = data_encoded.drop("whoAmI", axis=1)
 
 data_encoded.head()
+
+print("Преобразованный DataFrame:")
+print(data_encoded)
